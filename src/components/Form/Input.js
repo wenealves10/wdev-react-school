ecb4387acf96b2022c671327328032504c285424
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-23 19:41:35 -0300

    Input with Unform

---
+++ src/components/Form/Input.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
+import PropTypes from 'prop-types';
+
+export default function Input({ name, ...rest }) {
+  const inputRef = useRef(null);
+  const { fieldName, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return <input ref={inputRef} {...rest} />;
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+};

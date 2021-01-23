c399c8a566c6e2e8c4e53e2b104cbb04f49fe6bd
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-23 19:42:44 -0300

    New Styled for Unform

--- src/pages/Login/Styled.js
+++ src/pages/Login/Styled.js
@@ -1,12 +1,28 @@
 import styled from 'styled-components';
+import { Form as Unform } from '@unform/web';
+import * as colors from '../../config/colors';
 
-export const Title = styled.h1`
-  font-size: 1.6em;
+export const Form = styled(Unform)`
+  display: flex;
+  flex-direction: column;
+  span {
+    font-size: 1.2em;
+    font-weight: bold;
+    align-self: flex-start;
+  }
+  input {
+    margin-bottom: 8px;
+    padding: 0 15px;
+    font-size: 1em;
+    font-weight: 600;
+    width: 100%;
+    height: 30px;
+    outline: none;
+    border-color: ${colors.secondaryColor};
+  }
 `;
-
 export const Button = styled.button`
   width: 100%;
-  height: 40px;
-  margin-top: 18px;
-  margin-bottom: 20px;
+  height: 30px;
+  background-color: ${colors.primaryColor};
 `;

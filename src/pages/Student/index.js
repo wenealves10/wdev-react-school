f196f3cc9ec91a98bb53fef1bad61f5226795263
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-23 10:26:40 -0300

    Index Student

---
+++ src/pages/Student/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Title } from './Styled';
+import { Container } from '../../styles/Global';
+
+export default function Student() {
+  return (
+    <Container>
+      <Title>Student</Title>
+    </Container>
+  );
+}

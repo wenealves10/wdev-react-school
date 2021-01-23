826f134125e790140128f4bb5b045fa6d3d55e92
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-23 10:25:49 -0300

    Index Register

---
+++ src/pages/Register/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Title } from './Styled';
+import { Container } from '../../styles/Global';
+
+export default function Register() {
+  return (
+    <Container>
+      <Title>Register</Title>
+    </Container>
+  );
+}

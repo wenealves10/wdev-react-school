9454d8f0f2ad9b4bdc49e5f2718acbf83250ad77
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-23 13:20:29 -0300

    Adding the Form of Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { Title } from './Styled';
+import { FaUserCircle, FaSignInAlt } from 'react-icons/fa';
+import { Button, Title } from './Styled';
 import { Container } from '../../styles/Global';
+import Forms from '../../components/Form';
 
 export default function Login() {
   return (
     <Container>
-      <Title>Login</Title>
+      <Title>
+        <FaUserCircle size={50} />
+      </Title>
+      <Forms>
+        <span>E-mail</span>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite seu e-mail"
+        />
+        <span>Senha</span>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Digite sua senha"
+        />
+        <Button type="submit">
+          <FaSignInAlt size={30} />
+        </Button>
+      </Forms>
     </Container>
   );
 }

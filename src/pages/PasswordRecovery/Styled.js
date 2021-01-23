42b7ca5c7bcac5747849545c4bb8531b09affaac
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-23 10:23:38 -0300

    Styled Password Recovery

---
+++ src/pages/PasswordRecovery/Styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 1.6em;
+`;
+
+export const Button = styled.button`
+  width: 100px;
+  height: 40px;
+  margin-top: 18px;
+  margin-bottom: 20px;
+`;

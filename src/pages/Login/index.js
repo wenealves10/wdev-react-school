168bf1fbdbea1f01e86070006bcc8995f34b0071
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-23 21:37:24 -0300

    Styled the Form Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,22 +1,47 @@
-import React from 'react';
-import { FaSignInAlt, FaUserCircle } from 'react-icons/fa';
-import { Button, Form } from './Styled';
+import React, { useState } from 'react';
+import { FaAt, FaLock, FaSignInAlt, FaUserCircle } from 'react-icons/fa';
+import { Button, Form, FaRegEye, FaRegEyeSlash } from './Styled';
 import { Container } from '../../styles/Global';
 import Input from '../../components/Form/Input';
-
+// FaRegEye
 export default function Login() {
+  const [eye, setEye] = useState(false);
   function handleSubmit(data) {
     // eslint-disable-next-line no-console
     console.log(data);
   }
+
+  function handleClick() {
+    setEye(!eye);
+  }
+
   return (
     <Container>
-      <FaUserCircle size={42} />
+      <FaUserCircle size={50} />
       <Form onSubmit={handleSubmit}>
-        <span>E-mail</span>
+        <span className="email">
+          <FaAt size={24} />
+        </span>
         <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <span>Senha</span>
-        <Input type="password" name="password" placeholder="Digite sua senha" />
+        <span className="password">
+          <FaLock size={24} />
+        </span>
+
+        {eye ? (
+          <FaRegEyeSlash
+            className="eye-slash"
+            size={24}
+            onClick={handleClick}
+          />
+        ) : (
+          <FaRegEye size={24} className="eye-slash" onClick={handleClick} />
+        )}
+
+        <Input
+          type={eye ? 'text' : 'password'}
+          name="password"
+          placeholder="Digite sua senha"
+        />
         <Button type="submit">
           <FaSignInAlt size={30} />
         </Button>

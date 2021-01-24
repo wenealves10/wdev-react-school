26d5edde4aa2c6393f4ef6cf6c217ceefaad7201
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-23 22:53:19 -0300

    Validation of Form with Yup

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { FaAt, FaLock, FaSignInAlt, FaUserCircle } from 'react-icons/fa';
+import * as Yup from 'yup';
 import { Button, Form, FaRegEye, FaRegEyeSlash } from './Styled';
 import { Container } from '../../styles/Global';
 import Input from '../../components/Form/Input';
 // FaRegEye
 export default function Login() {
   const [eye, setEye] = useState(false);
-  function handleSubmit(data) {
-    // eslint-disable-next-line no-console
-    console.log(data);
+  const formRef = useRef(null);
+  async function handleSubmit(data, { reset }) {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('E-mail Inválido')
+          .required('E-mail é Obrigatório'),
+        password: Yup.string()
+          .min(4, 'No mínimo 4 caracteres')
+          .required('Senha Obrigatória'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+      formRef.current.setErrors({});
+      reset();
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errorMessages = {};
+        error.inner.forEach((erro) => {
+          errorMessages[erro.path] = erro.message;
+        });
+        formRef.current.setErrors(errorMessages);
+      }
+    }
   }
 
   function handleClick() {
@@ -18,7 +41,7 @@ export default function Login() {
   return (
     <Container>
       <FaUserCircle size={50} />
-      <Form onSubmit={handleSubmit}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <span className="email">
           <FaAt size={24} />
         </span>

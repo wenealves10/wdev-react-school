8be697dc7f6e6e1fe6b92db862afa0e978d1fe36
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-23 21:37:39 -0300

    Styled Of Login

--- src/pages/Login/Styled.js
+++ src/pages/Login/Styled.js
@@ -1,26 +1,60 @@
 import styled from 'styled-components';
 import { Form as Unform } from '@unform/web';
+import { FaRegEye as Eye, FaRegEyeSlash as NoEye } from 'react-icons/fa';
 import * as colors from '../../config/colors';
 
 export const Form = styled(Unform)`
   display: flex;
   flex-direction: column;
-  span {
+  position: relative;
+  span.email {
     font-size: 1.2em;
     font-weight: bold;
     align-self: flex-start;
+    position: absolute;
   }
+  span.password {
+    font-size: 1.2em;
+    font-weight: bold;
+    align-self: flex-start;
+    position: absolute;
+    top: 59px;
+  }
+
   input {
-    margin-bottom: 8px;
-    padding: 0 15px;
-    font-size: 1em;
+    margin-bottom: 30px;
+    padding: 0px 15px 0px 30px;
+    font-size: 0.8em;
     font-weight: 600;
     width: 100%;
     height: 30px;
     outline: none;
-    border-color: ${colors.secondaryColor};
+    border-top: none;
+    border-left: none;
+    border-right: none;
   }
 `;
+
+export const FaRegEye = styled(Eye)`
+  font-size: 1.2em;
+  font-weight: bold;
+  align-self: flex-start;
+  position: absolute;
+  top: 59px;
+  right: 10px;
+  cursor: pointer;
+`;
+
+export const FaRegEyeSlash = styled(NoEye)`
+  font-size: 1.2em;
+  font-weight: bold;
+  align-self: flex-start;
+  position: absolute;
+  top: 59px;
+  right: 10px;
+  cursor: pointer;
+`;
+
 export const Button = styled.button`
   width: 100%;
   height: 30px;

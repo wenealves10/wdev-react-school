a39306cbf65ecb6f2d35f221491b304b555ea01a
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-25 11:29:36 -0300

    Clear the code bad

--- src/store/modules/Authentication/reducer.js
+++ src/store/modules/Authentication/reducer.js
@@ -1,24 +1,18 @@
-/* eslint-disable no-console */
 import * as types from '../types';
 
 const initialState = {
-  buttonClick: false,
+  dataUser: {},
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case types.CLICK_BUTTON_SUCCESS: {
-      console.log('Success');
-      const newState = { ...state };
-      newState.buttonClick = !newState.buttonClick;
-      return newState;
+      return state;
     }
     case types.CLICK_BUTTON_FAILURE: {
-      console.log('Failure');
       return state;
     }
     case types.CLICK_BUTTON_REQUEST: {
-      console.log('Request');
       return state;
     }
 

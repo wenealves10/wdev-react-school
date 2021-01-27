90d11aaf31f89907a7a54d09f700dd0b58d06754
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-27 16:24:23 -0300

    Headers for Login and User Logged

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { FaUserLock } from 'react-icons/fa';
+import { FaHome, FaSignOutAlt, FaUserAlt, FaUserLock } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Nav } from './Styled';
+import * as actionsLogin from '../../store/modules/Authentication/actions';
 
-export default () => (
-  <Nav>
-    <Link to="/login">
-      <FaUserLock size={24} />
-      <span>Faça Login</span>
-    </Link>
-  </Nav>
-);
+export default function Header() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+  return (
+    <Nav>
+      {!isLoggedIn ? (
+        <Link to="/login">
+          <FaUserLock size={24} />
+          <span>Faça Login</span>
+        </Link>
+      ) : (
+        <>
+          <Link to="/">
+            <FaHome size={24} />
+            <span>Home</span>
+          </Link>
+          <Link to="/profile/user">
+            <FaUserAlt size={24} />
+            <span>Usuários</span>
+          </Link>
+          <Link
+            to="/login"
+            onClick={() => dispatch(actionsLogin.LoginFailure())}
+          >
+            <FaSignOutAlt size={24} />
+            <span>Logout</span>
+          </Link>
+        </>
+      )}
+    </Nav>
+  );
+}

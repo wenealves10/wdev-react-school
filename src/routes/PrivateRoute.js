effb701d0a5bd87f81bc3fea7570b5d69066556a
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-27 16:27:12 -0300

    Verifying if to be Logged

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 export default function PrivateRoute({
   component: Component,
   isAuthentication,
   ...rest
 }) {
-  const isLoggedIn = false;
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   if (isAuthentication && !isLoggedIn) {
     return (
       <Redirect

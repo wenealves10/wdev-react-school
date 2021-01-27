de8b4d8d7c841c873a7ce9bcb565a7042f3a7ccf
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-27 10:55:19 -0300

    deleted exemple de Saga

--- src/store/modules/Authentication/saga.js
+++ src/store/modules/Authentication/saga.js
@@ -1,28 +1,11 @@
-import { call, put, takeLatest, all } from 'redux-saga/effects';
-import { toast } from 'react-toastify';
-import * as actionsButton from './actions';
+import { takeLatest, all } from 'redux-saga/effects';
+// call, put,
+// import { toast } from 'react-toastify';
+// import * as actionsButton from './actions';
 import * as types from '../types';
 
-const request = () =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 500);
-  });
-
-function* Authentication() {
-  try {
-    yield call(request);
-    yield put(actionsButton.clickButtonSuccess());
-    toast.success('Sucesso na Chamada', {
-      toastId: 1,
-    });
-  } catch (error) {
-    yield put(actionsButton.clickButtonFailure());
-    toast.error('Ocorreu um Erro', {
-      toastId: 1,
-    });
-  }
+function* Authentication({ payload }) {
+  yield console.log('SAGA', payload);
 }
 
-export default all([takeLatest(types.CLICK_BUTTON_REQUEST, Authentication)]);
+export default all([takeLatest(types.LOGIN_REQUEST, Authentication)]);

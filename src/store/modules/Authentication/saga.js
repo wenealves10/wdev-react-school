d28458adf8cb9947c8ba165a9821d64b29a22c89
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-27 16:28:38 -0300

    Reducer Saga Login

--- src/store/modules/Authentication/saga.js
+++ src/store/modules/Authentication/saga.js
@@ -1,12 +1,42 @@
-import { takeLatest, all } from 'redux-saga/effects';
-// call, put,
-// import { toast } from 'react-toastify';
-// import * as actionsButton from './actions';
+import { call, put, takeLatest, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import { get } from 'lodash';
+import * as actionsLogin from './actions';
 import * as types from '../types';
+import axios from '../../../services/axios';
+import history from '../../../services/history';
 
 function* Authentication({ payload }) {
-  // eslint-disable-next-line no-console
-  yield console.log('SAGA', payload);
+  try {
+    const { email, password } = payload;
+    const response = yield call(axios.post, '/auth/token', { email, password });
+    yield put(actionsLogin.LoginSuccess({ ...response.data }));
+    toast.success(`Usuário ${response.data.user.name} está logado!`);
+    axios.defaults.headers.Authorization = `Bearer ${response.data.user.token}`;
+    history.push(payload.prevPatch);
+  } catch (error) {
+    const status = get(error, 'response.status', []);
+    if (status === 400) {
+      toast.error('Senha Inválida', {
+        toastId: 'ErrorPassword',
+      });
+    }
+    if (status === 404) {
+      toast.error('Usuário não existe', {
+        toastId: 'ErrorEmail',
+      });
+    }
+    yield put(actionsLogin.LoginFailure());
+  }
 }
 
-export default all([takeLatest(types.LOGIN_REQUEST, Authentication)]);
+function persistHydrate({ payload }) {
+  const token = get(payload, 'auth.token', '');
+  if (!token) return;
+  axios.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, Authentication),
+  takeLatest(types.PERSIST_HYDRATE, persistHydrate),
+]);

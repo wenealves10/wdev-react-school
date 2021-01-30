06cd599387a50ed86de5188fafae0fcf46111355
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-30 09:44:53 -0300

    Profile User

--- src/pages/ProfileUser/index.js
+++ src/pages/ProfileUser/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Title } from './Styled';
+import React, { /* useState, useEffect, */ useRef } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { Title, Form } from './Styled';
 import { Container } from '../../styles/Global';
+import Input from '../../components/Form/Input';
 
 export default function ProfileUser() {
+  const formRef = useRef(null);
+
+  async function handleSubmit(data, { reset }) {
+    console.log(data);
+    reset();
+  }
+
   return (
     <Container>
-      <Title>Perfil</Title>
+      <Title>
+        <FaUser size={45} />
+      </Title>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Digite seu nome" />
+      </Form>
     </Container>
   );
 }

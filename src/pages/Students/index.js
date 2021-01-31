4cf64c7827ba1b4c25574cc3acb73554405d910e
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-01-31 10:27:50 -0300

    Added Loading

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -10,61 +10,72 @@ import {
   StudentProfilePhotograph,
   Photograph,
   Link,
+  Loading,
 } from './Styled';
 import { Container } from '../../styles/Global';
 import axios from '../../services/axios';
+import * as colors from '../../config/colors';
 
 export default function Students() {
   const [students, setStudents] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     async function getStudents() {
       try {
         const response = await axios.get('/student');
         setStudents(response.data.students);
+        setIsLoading(false);
       } catch (error) {
         toast.error('Ocorreu um erro!', {
           toastId: 'students',
         });
+        setIsLoading(false);
       }
     }
     getStudents();
   }, []);
 
   return (
-    <Container>
-      <Title>
-        <IoIosPeople size={60} title="Escola Wdev" />
-        <span>Alunos</span>
-      </Title>
-      <StudentsContainer>
-        {students.map((student) => (
-          <Student key={String(student.id)}>
-            <Photograph>
-              {student.profiles ? (
-                <StudentProfilePhotograph
-                  src={student.profiles.url}
-                  alt={student.profiles.filename}
-                />
-              ) : (
-                <FaUserGraduate size={35} />
-              )}
-            </Photograph>
-            <span className="name_email">
-              <strong>{student.name}</strong>
-              <strong>{student.email}</strong>
-            </span>
-            <span className="button">
-              <Link to={`/student/${student.id}`}>
-                <FaEdit size={18} />
-              </Link>
-              <Link to={`/student/${student.id}`}>
-                <TiUserDelete size={24} />
-              </Link>
-            </span>
-          </Student>
-        ))}
-      </StudentsContainer>
-    </Container>
+    <Loading
+      isLoading={isLoading}
+      backgroundColor={colors.backgroundColor}
+      foregroundColor={colors.foregroundColor}
+    >
+      <Container>
+        <Title>
+          <IoIosPeople size={60} title="Escola Wdev" />
+          <span>Alunos</span>
+        </Title>
+
+        <StudentsContainer>
+          {students.map((student) => (
+            <Student key={String(student.id)}>
+              <Photograph>
+                {student.profiles ? (
+                  <StudentProfilePhotograph
+                    src={student.profiles.url}
+                    alt={student.profiles.filename}
+                  />
+                ) : (
+                  <FaUserGraduate size={35} />
+                )}
+              </Photograph>
+              <span className="name_email">
+                <strong>{student.name}</strong>
+                <strong>{student.email}</strong>
+              </span>
+              <span className="button">
+                <Link to={`/student/${student.id}`}>
+                  <FaEdit size={18} />
+                </Link>
+                <Link to={`/student/${student.id}`}>
+                  <TiUserDelete size={24} />
+                </Link>
+              </span>
+            </Student>
+          ))}
+        </StudentsContainer>
+      </Container>
+    </Loading>
   );
 }

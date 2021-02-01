590d9fcf10cf2bb4a49ada4ea5c12edcc025fb56
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-01 09:44:10 -0300

    Added Logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { FaHome, FaSignOutAlt, FaUserAlt, FaUserLock } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { Nav } from './Styled';
+import history from '../../services/history';
 import * as actionsLogin from '../../store/modules/Authentication/actions';
 
 export default function Header() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const dispatch = useDispatch();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actionsLogin.LoginFailure());
+    toast.success('Usuário Deslogado com sucesso!', {
+      toastId: 'logout',
+    });
+    history.push('/login');
+  };
+
   return (
     <Nav>
       {!isLoggedIn ? (
@@ -25,10 +37,7 @@ export default function Header() {
             <FaUserAlt size={24} />
             <span>Perfil</span>
           </Link>
-          <Link
-            to="/login"
-            onClick={() => dispatch(actionsLogin.LoginFailure())}
-          >
+          <Link to="/logout" onClick={handleLogout}>
             <FaSignOutAlt size={24} />
             <span>Logout</span>
           </Link>

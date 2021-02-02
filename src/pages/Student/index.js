96a07ef79679757e2d87c3a351c54c0f9bf49e97
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-02 18:38:24 -0300

    page of edit

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { Title } from './Styled';
 import { Container } from '../../styles/Global';
 
 export default function Student() {
+  const { id } = useParams();
   return (
     <Container>
-      <Title>Estudante</Title>
+      <Title>{id ? 'Editar Aluno' : 'Adicionar novo aluno'}</Title>
     </Container>
   );
 }

3aaaea0f240b38ebc62dc52236eda3d91d18701e
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-02 18:38:50 -0300

    removed path of photostudents

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,6 @@ import ForgotPassword from '../pages/ForgotPassword';
 import PasswordRecovery from '../pages/PasswordRecovery';
 import Students from '../pages/Students';
 import Student from '../pages/Student';
-import PhotosStudents from '../pages/PhotosStudents';
 import ReportStudent from '../pages/ReportStudent';
 import ProfileUser from '../pages/ProfileUser';
 import Page404 from '../pages/Page404';
@@ -16,12 +15,7 @@ export default function Routes() {
   return (
     <Switch>
       <PrivateRoute exact path="/" component={Students} isAuthentication />
-      <PrivateRoute path="/student/:id" component={Student} isAuthentication />
-      <PrivateRoute
-        path="/photos/student/:id"
-        component={PhotosStudents}
-        isAuthentication
-      />
+      <PrivateRoute path="/student/:id?" component={Student} isAuthentication />
       <PrivateRoute
         path="/report/student/:id"
         component={ReportStudent}

a58774a7d2fa3ee326291b75d1db3e2b3f47e2af
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-02 10:14:30 -0300

    added Button, Modal and Colors in Styled Components

--- src/pages/Students/Styled.js
+++ src/pages/Students/Styled.js
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
+import Rodals from 'rodal';
+import 'rodal/lib/rodal.css';
 import { Link as Links } from 'react-router-dom';
-import Loadings from 'styled-content-loader';
+import * as colors from '../../config/colors';
 
-export const Title = styled.div`
+export const Title = styled.h1`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  span {
-    font-size: 1.2em;
-    font-weight: bold;
-  }
+  font-size: 1.6em;
+  color: ${colors.primaryColor};
 `;
 
 export const StudentsContainer = styled.div`
@@ -33,8 +33,14 @@ export const Student = styled.div`
     align-items: center;
     justify-content: space-between;
     strong {
-      font-weight: normal;
+      font-weight: 500;
     }
+  }
+
+  span.actions {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
 `;
 
@@ -48,8 +54,37 @@ export const StudentProfilePhotograph = styled.img`
   border-radius: 50%;
 `;
 
+export const Button = styled.button`
+  width: 70%;
+  height: 40px;
+  background-color: ${colors.primaryColor};
+  padding: 5px;
+  margin: 0px auto 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 1.4em;
+    margin-right: 15px;
+  }
+`;
+
+export const Rodal = styled(Rodals)`
+  position: absolute;
+  p {
+    color: ${colors.primaryDarkColor};
+  }
+  div.button-options {
+    display: flex;
+    position: absolute;
+    bottom: 20px;
+    width: 100%;
+    button {
+      width: 30%;
+    }
+  }
+`;
+
 export const Link = styled(Links)`
   margin-left: 20px;
 `;
-
-export const Loading = styled(Loadings)``;

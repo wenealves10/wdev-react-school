035866e3b097483fb4ee7a5314004788c9441f18
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-05 09:05:56 -0300

    Styled of Photograph

--- src/components/Photograph/styled.js
+++ src/components/Photograph/styled.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
-// import * as colors from '../../config/colors';
+import { FaEdit as FaEditt } from 'react-icons/fa';
+import * as colors from '../../config/colors';
+
+export const Container = styled.div`
+  &:hover {
+    div.button-edit-student {
+      color: #fff;
+    }
+  }
+`;
 
 export const Picture = styled.div`
   margin-top: 15px;
@@ -8,8 +17,31 @@ export const Photographs = styled.div`
   background-image: url(${(props) => props.src});
   background-position: center;
   background-repeat: no-repeat;
-  background-size: contain;
+  background-size: cover;
   width: 180px;
   height: 180px;
   border-radius: 50%;
+`;
+
+export const ActionEdit = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 500ms;
+  color: ${colors.secondaryColor};
+  input[type='file'] {
+    display: none;
+  }
+  label {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+`;
+
+export const FaEdit = styled(FaEditt)`
+  position: absolute;
+  bottom: 5px;
 `;

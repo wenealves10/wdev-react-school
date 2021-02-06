fa0d85f726d5b60c2a0784134adfb04501c88d13
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-06 18:44:12 -0300

    page  styled of student report

--- src/pages/ReportStudent/index.js
+++ src/pages/ReportStudent/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import { Title } from './Styled';
-import { Container } from '../../styles/Global';
+import { FaBookReader, FaClipboardCheck } from 'react-icons/fa';
+import {
+  Title,
+  Line,
+  Picture,
+  ContainerStudentReport,
+  Container,
+  StudentData,
+  StudentNames,
+  StudentCaracteres,
+} from './Styled';
 
 export default function ReportStudent() {
   return (
     <Container>
-      <Title>ReportStudent</Title>
+      <Title>
+        <FaBookReader size={40} />
+        <span>Boletim Escolar</span>
+      </Title>
+      <Line />
+      <ContainerStudentReport>
+        <Picture />
+        <StudentData>
+          <StudentNames>
+            <span className="name-student">Nome:</span>
+            <span className="surname-student">Sobrenome:</span>
+          </StudentNames>
+          <StudentCaracteres>
+            <span className="age-student">Idade:</span>
+            <span className="height-student">Altura:</span>
+            <span className="weight-student">Peso:</span>
+          </StudentCaracteres>
+          <span className="email-student">E-mail</span>
+        </StudentData>
+      </ContainerStudentReport>
+      <Title>
+        <FaClipboardCheck size={30} />
+        <span>Disciplinas</span>
+      </Title>
     </Container>
   );
 }

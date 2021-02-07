6e447234b98358c6ffb5a37dd2c84a4f22594343
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-02-07 20:10:17 -0300

    added student data

--- src/pages/ReportStudent/index.js
+++ src/pages/ReportStudent/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { FaBookReader, FaClipboardCheck } from 'react-icons/fa';
+import { get } from 'lodash';
+import { toast } from 'react-toastify';
 import {
   Title,
   Line,
@@ -10,8 +13,28 @@ import {
   StudentNames,
   StudentCaracteres,
 } from './Styled';
+import axios from '../../services/axios';
 
 export default function ReportStudent() {
+  const { id } = useParams();
+  const [student, setStudent] = useState(0);
+  const [picture, setPicture] = useState('');
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get(`student/${id}`);
+        setStudent(get(data, 'student', {}));
+        setPicture(get(data, 'student.profiles.url', ''));
+      } catch (error) {
+        toast.error('Error ao carregar dados!', {
+          toastId: 'error',
+        });
+      }
+    };
+    getData();
+  }, [id]);
+
   return (
     <Container>
       <Title>
@@ -20,18 +43,20 @@ export default function ReportStudent() {
       </Title>
       <Line />
       <ContainerStudentReport>
-        <Picture />
+        <Picture src={picture} />
         <StudentData>
           <StudentNames>
-            <span className="name-student">Nome:</span>
-            <span className="surname-student">Sobrenome:</span>
+            <span className="name-student">Nome: {student.name}</span>
+            <span className="surname-student">
+              Sobrenome: {student.surname}
+            </span>
           </StudentNames>
           <StudentCaracteres>
-            <span className="age-student">Idade:</span>
-            <span className="height-student">Altura:</span>
-            <span className="weight-student">Peso:</span>
+            <span className="age-student">Idade: {student.age} anos</span>
+            <span className="height-student">Altura: {student.height} M</span>
+            <span className="weight-student">Peso: {student.weight} Kg</span>
           </StudentCaracteres>
-          <span className="email-student">E-mail</span>
+          <span className="email-student">E-mail: {student.email}</span>
         </StudentData>
       </ContainerStudentReport>
       <Title>
